7b930e2a4477455be47eaea24a8c9cd03df070ce
GerboBalazs <gerbovits.bali@gmail.com>
2023-10-27 15:32:11 +0200

    💡 Adds comment

--- src/api/controllers/controller.js
+++ src/api/controllers/controller.js
@@ -26,12 +26,15 @@ module.exports = {
     //To authenticate the user
     authenticate: async (req, res) => {
         await sql
+            //Check if the email is already exsisting
             .runQuery(`SELECT * FROM Users WHERE Email ='${req.body.email}'`)
             .then((result) => {
                 if (result.rowsAffected[0] == 0) return res.status(401).send({ msg: 'Email or password is incorrect!' });
+                //Check if the password is correct
                 if (result.recordset[0].Password !== createHash('sha256').update(req.body.password).digest('hex')) {
                     return res.status(401).send({ msg: 'Email or password is incorrect!' });
                 }
+                //If OK, then create token
                 const accessToken = jwt.sign(
                     { email: result.recordset[0].Email, userId: result.recordset[0].UserID },
                     process.env.ACCESS_TOKEN_SECRET,
@@ -69,6 +72,7 @@ module.exports = {
     getProduct: async (req, res) => {
         if ((await authorize(req, res)) === true) {
             try {
+                //Get the right product
                 const product = (
                     await sql.runQuery(
                         `SELECT p.Barcode, p.ImageLink,p.Name, d.Price, d.Discount, s.ShopName, s.ShopID FROM Products p
@@ -78,10 +82,12 @@ module.exports = {
                 const shops = (await sql.runQuery(`SELECT * FROM Shop`)).recordset;
                 let prices = [];
                 let availableShops = [];
+                //If the product is exsisting in more than one store
                 for (let element of product) {
                     prices.push({ ShopName: element.ShopName, Price: element.Price, ShopID: element.ShopID, Discount: element.Discount });
                     availableShops.push(element.ShopName);
                 }
+                //Set price and discount to 0 in unavailable shops
                 for (let shop of shops) {
                     if (!availableShops.includes(shop.ShopName)) {
                         prices.push({ ShopName: shop.ShopName, Price: 0, ShopID: shop.ShopID, Discount: 0 });
